d0b593a8c9d406633c333a4b510d3b265733f3e5
Matthew Turner <turnmas35@gmail.com>
2023-08-24 18:06:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(arrayOfStringNames, eventName) {
+  const messages = []
+  for (let i = 0; i < arrayOfStringNames.length; i++) {
+    messages.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return (messages)
+}
+
+writeCards(["Sam", "Luis", "Ted"], "Graduation")
+
+
+function countDown() {
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+}
